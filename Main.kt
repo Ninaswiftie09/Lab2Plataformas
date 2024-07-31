b41523db93bea10a8f0ed8202fe8cc59aa7fe7c6
bastmre44 <134813711+bastmre44@users.noreply.github.com>
2024-07-30 23:18:40 -0600

    Update Main.kt
    funciones que faltaban

--- Main.kt
+++ Main.kt
@@ -1,10 +1,13 @@
 import java.util.Stack
+import kotlin.math.pow
+import kotlin.math.exp
 
 fun precedence(op: Char): Int {
     return when (op) {
         '+', '-' -> 1
         '*', '/' -> 2
         '^' -> 3
+        'r', 'e' -> 4  // 'r' for root, 'e' for exponential
         else -> 0
     }
 }
@@ -23,8 +26,8 @@ fun evaluate(expression: String): Double {
                 continue
             }
 
-            c.isDigit() -> {
-                var buffer = StringBuilder()
+            c.isDigit() || c == '.' -> {
+                val buffer = StringBuilder()
                 while (i < expression.length && (expression[i].isDigit() || expression[i] == '.')) {
                     buffer.append(expression[i++])
                 }
@@ -41,7 +44,7 @@ fun evaluate(expression: String): Double {
                 ops.pop()
             }
 
-            c == '+' || c == '-' || c == '*' || c == '/' || c == '^' -> {
+            c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == 'r' || c == 'e' -> {
                 while (ops.isNotEmpty() && precedence(ops.peek()) >= precedence(c)) {
                     values.push(applyOp(ops.pop(), values.pop(), values.pop()))
                 }
@@ -64,12 +67,12 @@ fun applyOp(op: Char, b: Double, a: Double): Double {
         '-' -> a - b
         '*' -> a * b
         '/' -> a / b
-        '^' -> Math.pow(a, b)
+        '^' -> a.pow(b)
+        'r' -> b.pow(1.0 / a) // a root of b, usually written as b^(1/a)
+        'e' -> exp(a) // F.E
         else -> throw UnsupportedOperationException("Sintaxis incorrecta")
     }
 }
-
-
 
 fun main() {
     println("Ingrese la operación que desea realizar: ")
